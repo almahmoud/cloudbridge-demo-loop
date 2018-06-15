1f72c0a7a57b7b9b7448c6d4f8394a49e056921d
Alexandru Mahmoud <amahmou4@jhu.edu>
2018-06-15 15:24:53 -0400

    Removing hidden files

--- demo-instances.py
+++ demo-instances.py
@@ -9,6 +9,7 @@ keys/ directory, as well as create a CSV file containing all the information.
 This script can be run from command-line as follows:
 python3 demo-instances.py -n [num-instances]
 Additional parameters include:
+-s [int] : number to start at for naming instances (counting from 0 by default)
 -p prefix : specify a new prefix for all components and instances
 '''
 
@@ -24,178 +25,187 @@ import time
 
 args = sys.argv
 
-# Using a configuration file or environment variables by default.
-# Specify configuration dictionary here if using it instead
-config = {}
-
-# Each instance will have a randomly generated password of this size
-pw_size = 8  # chars
-pw_contents = string.ascii_lowercase + string.ascii_uppercase + string.digits
-
-# Ubuntu 16.04.03 @ Jetstream
-image_id = 'acb53109-941f-4593-9bf8-4a53cb9e0739'
-
-# Prefix used for naming all networking components and files
-prefix = '2018-gcc-training-'
-
-# The universal private key will be created in a file with this name
-kp_name = prefix + 'masterkey'
-kp_file = kp_name + '.pem'
-
-# Connecting to provider and generating keypair for all instances
-prov = CloudProviderFactory().create_provider(ProviderList.OPENSTACK, config)
-
-kp_find = prov.security.key_pairs.find(name=kp_name)
-if len(kp_find) > 0:
-    kp = kp_find[0]
-
-else:
-    kp = prov.security.key_pairs.create(kp_name)
-
-    # Some software (eg: paramiko) require that RSA be specified
-    key_contents = kp.material
-    if 'RSA PRIVATE' not in key_contents:
-        key_contents = key_contents.replace('PRIVATE KEY', 'RSA PRIVATE KEY')
-
-    # Writing private portion of key to .pem file
-    with open(kp_file, 'w') as f:
-        f.write(key_contents)
-    os.chmod(kp_file, 0o400)
-
-# Getting already existing network or creating a new one
-net_name = prefix + 'network'
-net_find = prov.networking.networks.find(name=net_name)
-if len(net_find) > 0:
-    net = net_find[0]
-else:
-    net = prov.networking.networks.create(
-        name=net_name, cidr_block='10.0.0.0/16')
-
-# Getting already existing subnet or creating a new one
-sn_name = prefix + 'subnet'
-sn_find = prov.networking.subnets.find(name=sn_name)
-if len(sn_find) > 0:
-    sn = sn_find[0]
-else:
-    sn = net.create_subnet(name=sn_name, cidr_block='10.0.0.0/25')
-
-# Getting already existing router or creating a new one
-router_name = prefix + 'router'
-router_find = prov.networking.routers.find(name=router_name)
-if len(router_find) > 0:
-    router = router_find[0]
-else:
-    router = prov.networking.routers.create(network=net, name=router_name)
-    router.attach_subnet(sn)
-
-gateway = net.gateways.get_or_create_inet_gateway(prefix + 'gateway')
-router.attach_gateway(gateway)
-
-# Getting already existing firewall or creating a new one
-fw_name = prefix + 'firewall'
-fw_find = prov.security.vm_firewalls.find(name=fw_name)
-if len(fw_find) > 0:
-    fw = fw_find[0]
-else:
-    fw = prov.security.vm_firewalls.create(fw_name, 'Instances for demo', net.id)
-    # Opening up the appropriate ports
-    fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 220, 220, '0.0.0.0/0')
-    fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 21, 21, '0.0.0.0/0')
-    fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 22, 22, '0.0.0.0/0')
-    fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 80, 80, '0.0.0.0/0')
-    fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 8080, 8080, '0.0.0.0/0')
-    fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 30000, 30100, '0.0.0.0/0')
-
-# Get image using the hardcoded ID
-img = prov.compute.images.get(image_id)
-
-# Get m1.small VM type (hardcoded), and print to make sure it is the desired one
-vm_type = [t for t in prov.compute.vm_types][4]
-print('VM Type used: ')
-print(vm_type)
-
-
-def create_instances(n):
-    '''
-    Creates the indicated number of instances after initialization.
-    '''
-    inst_names = []
-    inst_ids = []
-    inst_ips = []
-
-    for i in range(n):
-        print('\nCreating Instance #' + str(i))
-        curr_name = prefix + str(i)
-        inst = prov.compute.instances.create(
-            name=curr_name, image=img, vm_type=vm_type,
-            subnet=sn, key_pair=kp, vm_firewalls=[fw])
-        inst.wait_till_ready()  # This is a blocking call
-
-        # Track instances for immediate clean-up if requested
-        inst_names.append(curr_name)
-        inst_ids.append(inst.id)
-
-        # Get an available or create a floating IP, then attach it and print
-        # the public ip
-        fip = None
-        for each_fip in gateway.floating_ips.list():
-            if not each_fip.in_use:
-                fip = each_fip
-                break
-
-        if fip is None:
-            fip = gateway.floating_ips.create()
-
-        inst.add_floating_ip(fip)
-        inst.refresh()
-        inst_ip = str(inst.public_ips[0])
-        print('Instance Public IP: ' + inst_ip)
-
-        inst_ips.append(inst_ip)
-
-        print('Instance #' + str(i) + ' created.')
-
-    print(str(n) + ' instances were successfully created.')
-    return inst_names, inst_ids, inst_ips
-
-
-def password_access(ips):
-    inst_passws = []
-    time.sleep(30)
-    for each_ip in ips:
-        # Generate the instance-specific password
-        inst_pass = ''.join(random.choice(pw_contents) for i in range(pw_size))
-        subprocess.run([
-            'ssh', '-o', 'StrictHostKeyChecking=no', '-o', 
-            'UserKnownHostsFile=/dev/null', '-i', kp_file,
-            'ubuntu@' + each_ip, 'sed -e \
-            s/"PasswordAuthentication no"/"PasswordAuthentication yes"/g \
-            /etc/ssh/sshd_config > gcc-temp.txt \
-            && sudo mv gcc-temp.txt /etc/ssh/sshd_config\
-            && sudo service ssh restart\
-            && echo "ubuntu:' + inst_pass + '" | sudo chpasswd\
-            && sudo pip install -U cryptography\
-            && sudo rm -rf /usr/lib/python2.7/dist-packages/OpenSSL\
-            && sudo rm -rf /usr/lib/python2.7/dist-packages/pyOpenSSL-0.15.1.egg-info\
-            && sudo pip install pyopenssl\
-            && sudo dpkg --configure -a\
-            && sudo shutdown -r now'])
-# The cryprography and openssl fixes are needed to run the demo at:
-# https://github.com/galaxyproject/dagobah-training/blob/2018-gccbosc/sessions/14-ansible/ex2-galaxy-ansible.md
-# in June 2018
-        inst_passws.append(inst_pass)
-    return inst_passws
-
-
+# Return an error if the number of instances is not specified
 if '-n' not in args:
     print('ERROR: The number of instances to create must be specified using the \
           "-n [int]"" argument.')
 
 else:
+    # Using a configuration file or environment variables by default.
+    # Specify configuration dictionary here if using it instead
+    config = {}
+
+    # Each instance will have a randomly generated password of this size
+    pw_size = 8  # chars
+    pw_contents = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+    # Ubuntu 16.04.03 @ Jetstream
+    image_id = 'acb53109-941f-4593-9bf8-4a53cb9e0739'
+
+    # Prefix used for naming all networking components and files
+    if '-p' not in args:
+        prefix = '2018-gcc-training-'
+
+    else:
+        p = args.index('-p') + 1
+        prefix = str(args[p])
+
+    if '-s' not in args:
+        start = 0
+
+    else:
+        s = args.index('-s') + 1
+        start = int(args[s])
+
+    # The universal private key will be created in a file with this name
+    kp_name = prefix + 'masterkey'
+    kp_file = kp_name + '.pem'
+
+    # Connecting to provider and generating keypair for all instances
+    prov = CloudProviderFactory().create_provider(ProviderList.OPENSTACK, config)
+
+    kp_find = prov.security.key_pairs.find(name=kp_name)
+    if len(kp_find) > 0:
+        kp = kp_find[0]
+
+    else:
+        kp = prov.security.key_pairs.create(kp_name)
+
+        # Some software (eg: paramiko) require that RSA be specified
+        key_contents = kp.material
+        if 'RSA PRIVATE' not in key_contents:
+            key_contents = key_contents.replace('PRIVATE KEY', 'RSA PRIVATE KEY')
+
+        # Writing private portion of key to .pem file
+        with open(kp_file, 'w') as f:
+            f.write(key_contents)
+        os.chmod(kp_file, 0o400)
+
+    # Getting already existing network or creating a new one
+    net_name = prefix + 'network'
+    net_find = prov.networking.networks.find(name=net_name)
+    if len(net_find) > 0:
+        net = net_find[0]
+    else:
+        net = prov.networking.networks.create(
+            name=net_name, cidr_block='10.0.0.0/16')
+
+    # Getting already existing subnet or creating a new one
+    sn_name = prefix + 'subnet'
+    sn_find = prov.networking.subnets.find(name=sn_name)
+    if len(sn_find) > 0:
+        sn = sn_find[0]
+    else:
+        sn = net.create_subnet(name=sn_name, cidr_block='10.0.0.0/25')
+
+    # Getting already existing router or creating a new one
+    router_name = prefix + 'router'
+    router_find = prov.networking.routers.find(name=router_name)
+    if len(router_find) > 0:
+        router = router_find[0]
+    else:
+        router = prov.networking.routers.create(network=net, name=router_name)
+        router.attach_subnet(sn)
+
+    gateway = net.gateways.get_or_create_inet_gateway(prefix + 'gateway')
+    router.attach_gateway(gateway)
+
+    # Getting already existing firewall or creating a new one
+    fw_name = prefix + 'firewall'
+    fw_find = prov.security.vm_firewalls.find(name=fw_name)
+    if len(fw_find) > 0:
+        fw = fw_find[0]
+    else:
+        fw = prov.security.vm_firewalls.create(fw_name, 'Instances for demo', net.id)
+        # Opening up the appropriate ports
+        fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 220, 220, '0.0.0.0/0')
+        fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 21, 21, '0.0.0.0/0')
+        fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 22, 22, '0.0.0.0/0')
+        fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 80, 80, '0.0.0.0/0')
+        fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 8080, 8080, '0.0.0.0/0')
+        fw.rules.create(resources.TrafficDirection.INBOUND, 'tcp', 30000, 30100, '0.0.0.0/0')
+
+    # Get image using the hardcoded ID
+    img = prov.compute.images.get(image_id)
+
+    # Get m1.small VM type (hardcoded), and print to make sure it is the desired one
+    vm_type = [t for t in prov.compute.vm_types][4]
+    print('VM Type used: ')
+    print(vm_type)
+
+    def create_instances(n):
+        '''
+        Creates the indicated number of instances after initialization.
+        '''
+        inst_names = []
+        inst_ids = []
+        inst_ips = []
+
+        for i in range(start, n + start):
+            print('\nCreating Instance #' + str(i))
+            curr_name = prefix + str(i)
+            inst = prov.compute.instances.create(
+                name=curr_name, image=img, vm_type=vm_type,
+                subnet=sn, key_pair=kp, vm_firewalls=[fw])
+            inst.wait_till_ready()  # This is a blocking call
+
+            # Track instances for immediate clean-up if requested
+            inst_names.append(curr_name)
+            inst_ids.append(inst.id)
+
+            # Get an available or create a floating IP, then attach it and print
+            # the public ip
+            fip = None
+            for each_fip in gateway.floating_ips.list():
+                if not each_fip.in_use:
+                    fip = each_fip
+                    break
+
+            if fip is None:
+                fip = gateway.floating_ips.create()
+
+            inst.add_floating_ip(fip)
+            inst.refresh()
+            inst_ip = str(inst.public_ips[0])
+            print('Instance Public IP: ' + inst_ip)
+
+            inst_ips.append(inst_ip)
+
+            print('Instance "' + curr_name + '"" created.')
+
+        print(str(n) + ' instances were successfully created.')
+        return inst_names, inst_ids, inst_ips
+
+    def password_access(ips):
+        inst_passws = []
+        time.sleep(30)
+        for each_ip in ips:
+            # Generate the instance-specific password
+            inst_pass = ''.join(random.choice(pw_contents) for i in range(pw_size))
+            subprocess.run([
+                'ssh', '-o', 'StrictHostKeyChecking=no', '-o', 
+                'UserKnownHostsFile=/dev/null', '-i', kp_file,
+                'ubuntu@' + each_ip, 'sed -e \
+                s/"PasswordAuthentication no"/"PasswordAuthentication yes"/g \
+                /etc/ssh/sshd_config > gcc-temp.txt \
+                && sudo mv gcc-temp.txt /etc/ssh/sshd_config\
+                && sudo service ssh restart\
+                && echo "ubuntu:' + inst_pass + '" | sudo chpasswd\
+                && sudo pip install -U cryptography\
+                && sudo rm -rf /usr/lib/python2.7/dist-packages/OpenSSL\
+                && sudo rm -rf /usr/lib/python2.7/dist-packages/pyOpenSSL-0.15.1.egg-info\
+                && sudo pip install pyopenssl\
+                && sudo shutdown -r now'])
+    # The cryprography and openssl fixes are needed to run the demo at:
+    # https://github.com/galaxyproject/dagobah-training/blob/2018-gccbosc/sessions/14-ansible/ex2-galaxy-ansible.md
+    # in June 2018
+            inst_passws.append(inst_pass)
+        return inst_passws
+
     n = args.index('-n') + 1
     names, ids, ips = create_instances(int(args[n]))
     pws = password_access(ips)
     table = '{},{},{},{}\n'
-    with open('info.txt', 'w') as info:
+    with open('info.txt', 'a') as info:
         for i in range(len(names)):
             info.write(table.format(names[i], ids[i], ips[i], pws[i]))
